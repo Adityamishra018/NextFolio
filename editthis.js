b67a54411f3b6ff88b1b3b1e45b917fc5cc7db72
Aditya Mishra <adi.adityamishra007@gmail.com>
2021-06-26 19:41:23 +0530

    first-commit

---
+++ editthis.js
@@ -0,0 +1,27 @@
+var site = {
+    name : 'Aditya Mishra',
+    job: 'Front-End Learner',
+    description: `Hi there! I am Aditya Mishra from Raipur, India.
+    I am currently pursuing Bachelor of Engineering in Computer Science.
+    I ❤️ to design Websites, UI/UX & Programming.`,
+    skills_title: ['Web Development', 'Design','Programming'],
+    skills: [['Angular','Django', 'ReactJS','Express'], ['Tumblr', 'Imgur','Adobe'],['C/C++','Java','Python', 'Javascript']],
+    skillp:[[50, 70 ,60, 80], [50, 60, 50], [90,80,90,80]],
+    social: {
+        github: 'https://github.com/Adityamishra018',
+        twitter: 'https://twitter.com/imadi2ya007',
+        //linkedin: 'https://www.linkedin.com/in/aditya/',
+        mail: 'mailto: adi.adityamishra@gmail.com',
+        instagram: 'https://www.instagram.com/?hl=en'
+    },
+    form: 'https://formsubmit.co/adi.adityamishra007@gmail.com',
+    projects: [
+        ["https://github.com/Adityamishra018", "https://github.com/Adityamishra018","https://github.com/Adityamishra018", "https://github.com/Adityamishra018"],
+         ['FASTag Simulation', 'Bionic Search Engine', "Tour Guide", "Portfolio Site"],
+        ['Java project that simulates FASTag Operation', 'Search Engine For Bio-mechanics', "C++ Tour Application", "Portfolio Site using NextJS"],
+        
+    ],
+   
+    
+}
+export default site;
